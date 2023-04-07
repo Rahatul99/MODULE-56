b2c88ea54774935d915b39e3b6623b8283c1cb9d
rahatul <rahatul99@gmail.com>
2023-04-08 01:22:38 +0600

    here is contex lengthy way

--- banglar-t-shirt/src/Components/Cart/Cart.jsx
+++ banglar-t-shirt/src/Components/Cart/Cart.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Tshirt from '../Tshirt/Tshirt';
+import './Cart.css';
 
 const Cart = ({cart, handleRemoveFromCart}) => {
     let message;
@@ -11,13 +12,23 @@ const Cart = ({cart, handleRemoveFromCart}) => {
     }
     return (
         <div>
-            <h2>Order summary: {cart.length}</h2>
+            <h2 className={cart.length === 1 ? 'blue' : 'green'}>Order summary: {cart.length}</h2>
+            <p className={`bold bordered ${cart.length === 3 ? 'green' : 'red'}`}>Conditional CSS class</p>
+            {cart.length > 2 ? 
+            <span className='purple'>Aro kino</span> 
+            : <span>fokira</span>}
             {message}
             {
                 cart.map(tshirt => <p
                      key={tshirt._id}>
                         {tshirt.name}
                         <button onClick={()=>handleRemoveFromCart(tshirt._id)}>X</button></p>)
+            }
+            {
+                cart.length === 2 && <p>Double Bonanza!!!</p>
+            }
+            {
+                cart.length !==3 || <h3>4 tah tho hoilo nah</h3>
             }
         </div>
     );
--- banglar-t-shirt/src/main.jsx
+++ banglar-t-shirt/src/main.jsx
@@ -9,6 +9,8 @@ import {
 import Home from './Components/Home/Home';
 import Main from './Components/Layout/Main';
 import OrderReview from './Components/OrderReview/OrderReview';
+import Grandpa from './Components/Grandpa/Grandpa';
+
 
 const router = createBrowserRouter([
   {
@@ -24,6 +26,10 @@ const router = createBrowserRouter([
         path: 'review',
         element: <OrderReview />
       },
+      {
+        path:'/grandpa',
+        element: <Grandpa />
+      }
     ]
   },
 ]);
